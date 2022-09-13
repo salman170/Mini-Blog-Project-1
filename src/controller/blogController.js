0c25e564948a4b32bead0658c99195847beb0e88
salman170 <salmanansari02120@gmail.com>
2022-09-14 02:25:41 +0530

    Done

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -2,19 +2,13 @@ const { default: mongoose } = require("mongoose")
 const authorModel = require("../model/authorModel")
 const blogModel = require("../model/blogModel")
 
-/*### POST /blogs
-- Create a blog document from request body. Get authorId in request body only.
-- Make sure the authorId is a valid authorId by checking the author exist in the authors collection.
-- Return HTTP status 201 on a succesful blog creation. Also return the blog document. The response should be a JSON object like [this](#successful-response-structure) 
-- Create atleast 5 blogs for each author
 
-- Return HTTP status 400 for an invalid request with a response body like [this](#error-response-structure)
-*/
 
 const validdetails = function (details) {
     return /^[a-zA-Z,'.\s]{0,150}$/.test(details)
 }
 
+//### POST /blogs
 const createBlog = async function (req, res) {
     try {
         let data = req.body
@@ -58,18 +52,8 @@ const createBlog = async function (req, res) {
     }
 }
 
-/*
-### GET /blogs
-- Returns all blogs in the collection that aren't deleted and are published
-- Return the HTTP status 200 if any documents are found. The response structure should be like [this](#successful-response-structure) 
-- If no documents are found then return an HTTP status 404 with a response like [this](#error-response-structure) 
-- Filter blogs list by applying filters. Query param can have any combination of below filters.
-  - By author Id
-  - By category
-  - List of blogs that have a specific tag
-  - List of blogs that have a specific subcategory
-example of a query url: blogs?filtername=filtervalue&f2=fv2
-*/
+
+// ### GET /blogs
 
 const getBlog = async function (req, res) {
     try {
@@ -108,12 +92,6 @@ const getBlog = async function (req, res) {
 
 
 // ### PUT /blogs/:blogId
-// - Updates a blog by changing the its title, body, adding tags, adding a subcategory. (Assuming tag and subcategory received in body is need to be added)
-// - Updates a blog by changing its publish status i.e. adds publishedAt date and set published to true
-// - Check if the blogId exists (must have isDeleted false). If it doesn't, return an HTTP status 404 with a response body like [this](#error-response-structure)
-// - Return an HTTP status 200 if updated successfully with a body like [this](#successful-response-structure) 
-// - Also make sure in the response you return the updated blog document. 
-
 
 const updateBlog = async function (req, res) {
     try {
